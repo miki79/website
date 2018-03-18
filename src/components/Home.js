63f3e05d25412f690cffec30c7f46fa96c7554cd
Michele Giardi <miki79@users.noreply.github.com>
2018-03-18 20:58:07 +0000

    Add twitter link

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import githubLogo from './github.png';
+import twitterLogo from './twitter.jpg';
 import './Home.css';
 
 class Home extends Component {
@@ -11,11 +12,12 @@ class Home extends Component {
             .
           </span>uk
         </h1>
-        <a href="https://github.com/miki79">
-          <img src={githubLogo} alt="Github" />github.com/<span className="yellow">
-            miki79
-          </span>
-        </a>
+        <p>
+          <a href="https://github.com/miki79"><img src={githubLogo} alt="Github" />github.com/<span className="yellow">miki79</span></a>
+        </p>
+        <p>          
+          <a href="https://twitter.com/mikiuk"><img src={twitterLogo} alt="Twitter" />twitter.com/<span className="yellow">mikiuk</span></a>
+        </p>
       </div>
     );
   }
