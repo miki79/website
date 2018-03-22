5ec9f3a06cf8e1247db3d11feed1c96ce948e96a
Michele Giardi <miki79@users.noreply.github.com>
2018-03-22 22:06:50 +0000

    Add background image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
 import ReactGA from 'react-ga';
 import githubLogo from './github.png';
-import twitterLogo from './twitter.jpg';
+import twitterLogo from './twitter.png';
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <h1 className="titleHome">miki<span className="yellow">.</span>me<span className="yellow">.</span>uk</h1>
+        <h1 className="titleHome">
+          miki<span className="yellow">.</span>me<span className="yellow">
+            .
+          </span>uk
+        </h1>
         <p>
-          <ReactGA.OutboundLink eventLabel="github" to="https://github.com/miki79">
-            <img src={githubLogo} alt="Github" />github.com/<span className="yellow">miki79</span>
+          <ReactGA.OutboundLink
+            eventLabel="github"
+            to="https://github.com/miki79"
+          >
+            <img src={githubLogo} alt="Github" />github.com/<span className="yellow">
+              miki79
+            </span>
           </ReactGA.OutboundLink>
         </p>
-        <p>          
-          <ReactGA.OutboundLink eventLabel="twitter" to="https://twitter.com/mikiuk">
-            <img src={twitterLogo} alt="Twitter" />twitter.com/<span className="yellow">mikiuk</span>
+        <p>
+          <ReactGA.OutboundLink
+            eventLabel="twitter"
+            to="https://twitter.com/mikiuk"
+          >
+            <img src={twitterLogo} alt="Twitter" />twitter.com/<span className="yellow">
+              mikiuk
+            </span>
           </ReactGA.OutboundLink>
         </p>
       </div>
